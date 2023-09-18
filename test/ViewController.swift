667d3d4b5afa7d2eccc8971a5b9ccf6d70272049
Dima <nudelman1995@mail.ru>
2023-09-18 15:14:10 +0300

    First commit

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Дмитрий Пономарев on 18.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+//    "here is mistake"
+}
+

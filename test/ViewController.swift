d345610af2c4992f0da5852982d4f7bc485255ed
Dima <nudelman1995@mail.ru>
2023-09-18 15:29:32 +0300

    oter code

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("i make mistake")
+        print("others developers code")
     }
     
     mistake
